22541f56ce78baa86b73427610662d7b8efceb93
Izabella Silva <izabellaspv@gmail.com>
2020-03-29 18:50:18 -0300

    frontend complete

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -47,7 +47,7 @@ module.exports = {
             .where('id', id)
             .select('ong_id')
             .first()
-        if (incident.org_id != ong_id) {
+        if (incident.ong_id != ong_id) {
             return response.status(401).json({ error: 'Operation not permited.' });
         }
 
